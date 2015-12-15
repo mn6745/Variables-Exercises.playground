d3aa0505e8055a87f5168dd474a076080c318acf
MarkJN41 <MarkNewell22@hotmail.com>
2015-12-15 15:10:38 -0500

    Complete
    Complete

---
+++ Contents.swift
@@ -0,0 +1,46 @@
+//: ## Variables Exercises
+//: In these exercises, you will create variables and constants, and you will handle simple compiler areas that arise when working with them.
+/*:
+### Exercise 1
+
+For each of the following names and types, create a variables or constant that best represents it:
+
+- boilingPointOfWaterInCelsius (Double)
+- gallonsRemainingInTank (Double)
+- naturalLogarithmicBase (Double)
+- ageOfSpecimenInWeeks (Int)
+- studentNickname (String)
+*/
+
+/*
+let boilingPointOfWaterInCelsius = 100.00
+var gallonsRemainingInTank = 6.25
+let naturalLogarithmicBase = 3.14
+var ageOfSpecimenInWeeks = 5
+let studentNickname = "Buddy"
+
+*/
+
+
+
+//: ### Exercise 2
+//: Fix the following compiler errors related to assignment, variables, and constants:
+let favoriteLanguage: String = "Swift"
+var gallonsRemainingInTank = 6.25
+var ageOfSpecimenInWeeks = 32.45
+let numberOfGasIcons: Int = 8
+var numberOfPolygonsInModel = 30
+numberOfPolygonsInModel = 32
+var polygonsLeftToDraw = 0
+polygonsLeftToDraw = numberOfPolygonsInModel
+
+//: ### Exercise 3
+//: Fix the following compiler errors related to invalid names:
+var nameOfSchool = "Udacity"
+var subject = "Math"
+let type = "Nissian"
+var 🐩myDog = "🐶"
+var 🐕yourDog = "🐶"
+
+
+
